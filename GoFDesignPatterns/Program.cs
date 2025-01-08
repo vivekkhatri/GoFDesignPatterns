115b9b62d38383b073db27e1249ee6284bbb0915
Vivek Khatri <vicky.khatri42@gmail.com>
2025-01-08 08:38:27 -0500

    Add project files.

---
+++ GoFDesignPatterns/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GoFDesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
